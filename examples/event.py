2de8e124aaaa04332acc6d1780cad44a64bebe40
KirisameVanilla <vanillaaaajp@gmail.com>
2025-02-03 11:32:45 +0800

    revert: "fix: 更改描述; 新增event.py中自定义plan_folder从而生成绝对路径."
    This reverts commit b3188d9f6f2d94f05302be38e32e03dffa16839c.

--- examples/event.py
+++ examples/event.py
@@ -9,10 +9,8 @@ plan = EventFightPlan20250123(
     plan_path='H1AB炸鱼',
     fleet_id=4,
 )
-# plan_path处可填写plan的yaml文件的绝对地址
-# 若运行目录在examples文件夹, 也可使用相对地址直接填写plan的文件名
-# 或者可以填写plan_folder的绝对路径, 然后plan_path填写plan的文件名
-# 详细的plan名可在plans/event/20250123查看，fleet_id为出击编队
+# plan_path处可填写plan的yaml文件的绝对地址, 也可使用相对地址直接填写plan的文件名
+# 详细的plan名可在data/plans/event/20250123查看，fleet_id为出击编队
 
 
 plan.run_for_times(
